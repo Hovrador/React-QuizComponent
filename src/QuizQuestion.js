22f4fdd1f27373bdc79c0cd612c56db8ff0a5f47
Hovrador <Hovrador@users.noreply.github.com>
2020-03-24 17:59:57 -0500

    Pluralsight check 3.7

---
+++ src/QuizQuestion.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class QuizQuestion extends Component{
+    render(){
+        return(
+            <main>
+                <section>
+                    <p>
+                        //instruction text goes here
+                    </p>
+                </section>
+                <section>
+                    <ul>
+                        //quiz question goes here
+                    </ul>
+                </section>
+            </main>
+        );
+    }
+};
+
+export default QuizQuestion;
