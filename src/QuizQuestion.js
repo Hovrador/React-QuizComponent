ca4020a2e49d4aa445600ce7947b04d66971b54c
Hovrador <Hovrador@users.noreply.github.com>
2020-03-24 19:51:09 -0500

    Pluralsight check 6.8

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component{
-    constructor(props){
-        super(props)
+    handleClick(buttonText){
+        if(buttonText === this.props.quiz_question.answer){
+            this.props.showNextQuestionHandler()
+        }
     }
     render(){
         return(
@@ -14,16 +16,13 @@ class QuizQuestion extends Component{
                     </p>
                 </section>
                 <section>
-                    {this.props.quiz_question.answer_options.map(index => {
-                        return(
-                            <QuizQuestionButton key = {index} button_text = {this.props.quiz_question.answer_options[index]}/>
-                        )
-                    })};
-                    {/* <ul>
-                        <li>
-                            <QuizQuestionButton button_text = {this.props.quiz_question.answer_options[0]}/>
-                        </li>
-                    </ul> */}
+                    <ul>
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                            return(
+                                <QuizQuestionButton key = {index} button_text = {answer_option} clickHandler = {this.handleClick.bind(this)}/>
+                            )
+                        })}
+                    </ul>
                 </section>
             </main>
         );
